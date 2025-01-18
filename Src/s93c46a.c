1e5c8779ac7dadb444096532d863255183a74e58
wataoxp <168703313+wataoxp@users.noreply.github.com>
2025-01-18 19:37:54 +0900

    Update s93c46a.c

--- Src/s93c46a.c
+++ Src/s93c46a.c
@@ -5,47 +5,86 @@
  *      Author: wataoxp
  */
 #include "s93c46.h"
-#include "delay.h"
-#include "gpio.h"
 
-//#define ThreeWire
+#define ThreeWire
 
+static uint8_t CheckPosition(uint16_t val);			//POSITION_VALの代わり
 static void SendBit(uint8_t bit);
 static void SendOpCode(uint8_t code);
 static void WriteAddress(uint8_t add);
 static void WriteData(uint16_t data);
 static uint16_t ReadData(void);
+static void Verify(void);
 
+static GPIO_TypeDef* CS_Port;
+static GPIO_TypeDef* SK_Port;
+static GPIO_TypeDef* DI_Port;
+static GPIO_TypeDef* DO_Port;
+
+static uint16_t Select;
+static uint16_t Clock;
+static uint16_t Output;
+static uint16_t Input;
+
+static uint8_t DIshift;
+static uint8_t DOshift;
+
+void SetHandle(S93C46_Typedef* init)
+{
+	CS_Port = init->PortCS;
+	SK_Port = init->PortSK;
+	DI_Port = init->PortDI;
+	DO_Port = init->PortDO;
+
+	Select = init->CS;
+	Clock = init->SK;
+	Output = init->DI;
+
+	DIshift = CheckPosition(init->DI);
+	DOshift = CheckPosition(init->DO);
+}
+static uint8_t CheckPosition(uint16_t val)
+{
+	uint8_t i;
+	for(i = 0; i < 16;i++)
+	{
+		if(((val >> i) & 0x01) == 1)
+		{
+			break;
+		}
+	}
+	return i;
+}
 static inline void DummyClock(void)
 {
-	WRITE_REG(GPIOA->BRR,DI);
-	WRITE_REG(GPIOA->BSRR,SK);
+	WRITE_REG(DI_Port->BRR,Output);
+	WRITE_REG(SK_Port->BSRR,Clock);
 	Delay(SK_TIME);
-	WRITE_REG(GPIOA->BRR,SK);
+	WRITE_REG(SK_Port->BRR,Clock);
 	Delay(SK_TIME);
 }
-static inline void StartBit(void)		//CSをHiにした後、DIをHiにすることでスタートビットになる。
+static inline void StartBit(void)					//CSをHiにした後、DIをHiにすることでスタートビットになる。
 {
 	SendBit(true);
 }
 static inline void EnableChip(void)
 {
-	WRITE_REG(GPIOA->BSRR,CS);
+	WRITE_REG(CS_Port->BSRR,Select);
 	Delay(1);										//wait tCSS
 }
 static inline void DisableChip(void)
 {
-	WRITE_REG(GPIOA->BRR,CS);
+	WRITE_REG(CS_Port->BRR,Select);
 	Delay(1);										//wait tCDS
 }
 static void SendBit(uint8_t bit)
 {
-	WRITE_REG(GPIOA->BSRR,(bit << DI_SHIFT));		//データをセット
+	WRITE_REG(DI_Port->BSRR,(bit << DIshift));			//データをセット
 	Delay(SK_TIME);
-	WRITE_REG(GPIOA->BSRR,SK);						//MX_GPIO_Initですべてのピンは初期状態がLow
+	WRITE_REG(SK_Port->BSRR,Clock);						//MX_GPIO_Initですべてのピンは初期状態がLow
 	Delay(SK_TIME);
-	WRITE_REG(GPIOA->BRR,DI);						//アイドル時はDI、SK共にLowにする
-	WRITE_REG(GPIOA->BRR,SK);
+	WRITE_REG(DI_Port->BRR,Output);						//アイドル時はDI、SK共にLowにする
+	WRITE_REG(SK_Port->BRR,Clock);
 }
 static void SendOpCode(uint8_t code)
 {
@@ -54,35 +93,48 @@ static void SendOpCode(uint8_t code)
 		SendBit((code >> i) & 0x01);
 	}
 }
+#ifdef ThreeWire
 static uint16_t ReadData(void)
 {
 	uint16_t ret;
-
-#ifdef ThreeWire
 	for(uint8_t i = 0; i < 16; i++)
 	{
 		ret <<= 1;
 		Delay(SK_TIME);
-		WRITE_REG(GPIOA->BSRR,SK);
+		WRITE_REG(SK_Port->BSRR,Clock);
 		Delay(SK_READ_TIME);
 
-		ret |= (GPIOA->IDR & DI) >> DI_SHIFT;
-		WRITE_REG(GPIOA->BRR,SK);
+		ret |= (DI_Port->IDR & Input) >> DIshift;
+		WRITE_REG(SK_Port->BRR,Clock);
 	}
-#else
-	for(uint8_t i = 0; i < 16; i++)
-	{
-		ret <<= 1;
-		Delay(SK_TIME);
-		WRITE_REG(GPIOA->BSRR,SK);
-		Delay(SK_READ_TIME);
-
-		ret |= (GPIOA->IDR & DO) >> DO_SHIFT;
-		WRITE_REG(GPIOA->BRR,SK);
-	}
-#endif
 	return ret;
 }
+static void Verify(void)
+{
+	Delay(5000);
+}
+#else
+static uint16_t ReadData(void)
+{
+	uint16_t ret;
+	for(uint8_t i = 0; i < 16; i++)
+	{
+		ret <<= 1;
+		Delay(SK_TIME);
+		WRITE_REG(SK_Port->BSRR,Clock);
+		Delay(SK_READ_TIME);
+
+		ret |= (DO_Port->IDR & Input) >> DOshift;
+		WRITE_REG(SK_Port->BRR,Clock);
+	}
+	return ret;
+}
+static void Verify(void)
+{
+	EnableChip();
+	while(!(DO_Port->IDR & Input));			//Check Busy
+}
+#endif
 static void WriteAddress(uint8_t add)			//いずれもMSBから送信
 {
 	for(int8_t i = 7; i >= 0; i--)
@@ -104,16 +156,15 @@ void WriteRom(uint8_t address,uint8_t code,uint16_t data)
 	WriteAddress(address | code);			//オペコード2ビット+アドレス
 	WriteData(data);
 	DisableChip();
+	Verify();
 }
 void ReadRom(uint8_t address,uint16_t *val)
 {
 	EnableChip();
 	StartBit();
 	WriteAddress(address | READ_CODE);
-	GPIO_init(GPIOA, DI, LL_GPIO_MODE_INPUT);
 	*val = ReadData();
 	DisableChip();
-	GPIO_init(GPIOA, DI, LL_GPIO_MODE_OUTPUT);
 }
 void EnableWrite(void)
 {
@@ -130,15 +181,5 @@ void DisableWrite(void)
 	StartBit();
 	SendOpCode(DISABLE_CODE);
 	DisableChip();
-	DeInit1usTick();				//SysTickを1msカウントに戻す
-}
-void Verify(void)
-{
-#ifdef ThreeWire
-	Delay(5000);
-#else
-	EnableChip();
-
-	while(!(GPIOA->IDR & DO));		//Check Busy
-#endif
+	DeInit1usTick();						//SysTickを1msカウントに戻す
 }
